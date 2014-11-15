5c09b02c1cf64a30191897fa39b0b0c78b3c412a
a.khoma <2kishh@gmail.com>
2014-11-15 19:28:29 +0200

    back and crud. CSS

--- sites/all/modules/ironfolio/ironfolio-admin-list.tpl.php
+++ sites/all/modules/ironfolio/ironfolio-admin-list.tpl.php
@@ -2,60 +2,68 @@
 global $base_path, $language_url;
 $current_url_path = $base_path;
 $current_url_path .=  $language_url->prefix ?  $language_url->prefix . '/' : '';
+$languages = language_list('enabled');
 ?>
 <div class="ironfolio-admin">
-    <h1>language select</h1>
-    <ul>
-        <li><a href="<?php echo $base_path . 'en/' . "admin/ironfolio/" . $current_cat_id ?>">English</a></li>
-        <li><a href="<?php echo $base_path . 'ru/' . "admin/ironfolio/" . $current_cat_id ?>">Русский</a></li>
-    </ul>
+    <fieldset id="edit-filters" class="form-wrapper">
+        <legend><span class="fieldset-legend">Выбор языка</span></legend>
+        <ul class="languages">
+        <?php foreach($languages[1] as $lang_item): ?>
+            <li><a class="<?php if ($lang_item->prefix == $language_url->prefix) echo 'active'; ?>"
+                   href="<?php  echo $base_path . $lang_item->prefix . '/' . "admin/ironfolio/" . $current_cat_id ?>">
+                    <?php echo $lang_item->native; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    </fieldset>
 
-
-
-    <h1>Cats</h1>
-    <ul>
-    <?php foreach ($categories as $cat): ?>
-        <?php
-            $cat_class = "";
-               if ($current_cat_id == $cat->tid) {
-                   $cat_class = 'active';
-               }
-        ?>
-        <li class="<?php echo $cat_class; ?>">
-            <a href="<?php echo $current_url_path . "admin/ironfolio/$cat->tid"; ?>"><?php echo $cat->name; ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-
-    <br />
-    <h1>Items</h1>
-    <ul class="folioitems">
-    <?php for($i = 0; $i < count($iron_folio_items); $i++): ?>
-        <?php
-            $item = $iron_folio_items[$i];
-            $prev_item = isset($iron_folio_items[$i-1]) ? $iron_folio_items[$i-1] : false;
-            $next_item = isset($iron_folio_items[$i+1]) ? $iron_folio_items[$i+1] : false;
-        ?>
-        <li>
+    <div id="edit-filters" class="admin-panel">
+        <h3>Категории</h3>
+        <ul class="categories">
+        <?php foreach ($categories as $cat): ?>
             <?php
-                echo $helper->renderFolioNodeFieldImage($item);
+                $cat_class = "";
+                   if ($current_cat_id == $cat->tid) {
+                       $cat_class = 'active';
+                   }
             ?>
-            <?php echo $item->title; ?>
-            <br />
-            <?php echo $item->field_sort_order['und'][0]['value']; ?>
-            <br />
-            <?php if ($prev_item): ?>
-                <a href="<?php echo $current_url_path . "admin/ironfolio/changesortorder/$item->nid/$prev_item->nid/"; ?>">Up</a>
-            <?php endif ?>
-            <br />
-            <?php if ($next_item): ?>
-                <a href="<?php echo $current_url_path . "admin/ironfolio/changesortorder/$item->nid/$next_item->nid/"; ?>">Down</a>
-            <?php endif ?>
-            <br />
-            <a href="<?php echo $current_url_path . "node/$item->nid/edit/?destination=admin/ironfolio/$current_cat_id/"; ?>">Edit</a>
-            <br />
-            <a href="<?php echo $current_url_path . "node/$item->nid/delete/?destination=admin/ironfolio/$current_cat_id/"; ?>">Delete</a>
-        </li>
-    <?php endfor; ?>
-    </ul>
+            <li>
+                <a class="<?php echo $cat_class; ?>" href="<?php echo $current_url_path . "admin/ironfolio/$cat->tid"; ?>"><?php echo $cat->name; ?></a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    </div>
+
+    <fieldset id="edit-filters" class="form-wrapper">
+        <legend><span class="fieldset-legend">Портфолио</span></legend>
+        <a class="button" href="<?php echo $current_url_path . "node/add/iron-folio/?destination=admin/ironfolio/$current_cat_id/"; ?>">Добавить работу</a>
+        <ul class="folioitems">
+        <?php for($i = 0; $i < count($iron_folio_items); $i++): ?>
+            <?php
+                $item = $iron_folio_items[$i];
+                $prev_item = isset($iron_folio_items[$i-1]) ? $iron_folio_items[$i-1] : false;
+                $next_item = isset($iron_folio_items[$i+1]) ? $iron_folio_items[$i+1] : false;
+            ?>
+            <li>
+                <?php
+                    echo $helper->renderFolioNodeFieldImage($item);
+                ?>
+                <?php // echo $item->field_sort_order['und'][0]['value']; ?>
+                <div class="folio-item-navigation">
+                    <?php if ($prev_item): ?>
+                        <a class="folio-up" href="<?php echo $current_url_path . "admin/ironfolio/changesortorder/$item->nid/$prev_item->nid/"; ?>"></a>
+                    <?php endif ?>
+
+                    <?php if ($next_item): ?>
+                        <a class="folio-down" href="<?php echo $current_url_path . "admin/ironfolio/changesortorder/$item->nid/$next_item->nid/"; ?>"></a>
+                    <?php endif ?>
+
+                    <a class="folio-edit" href="<?php echo $current_url_path . "node/$item->nid/edit/?destination=admin/ironfolio/$current_cat_id/"; ?>"></a>
+                    <a class="folio-delete" href="<?php echo $current_url_path . "node/$item->nid/delete/?destination=admin/ironfolio/$current_cat_id/"; ?>"></a>
+                </div>
+            </li>
+        <?php endfor; ?>
+        </ul>
+    </fieldset>
 </div>
