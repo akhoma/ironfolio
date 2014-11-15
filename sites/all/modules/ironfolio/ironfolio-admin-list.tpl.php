c9359ac952567710b5c5b6afd6e769b67ff26b77
a.khoma <2kishh@gmail.com>
2014-11-15 10:57:58 +0200

    Node sorting and langeage select in admin

--- sites/all/modules/ironfolio/ironfolio-admin-list.tpl.php
+++ sites/all/modules/ironfolio/ironfolio-admin-list.tpl.php
@@ -1,8 +1,16 @@
 <?php
 global $base_path, $language_url;
-$current_url_path = $base_path;  $language_url->prefix;
+$current_url_path = $base_path;
 $current_url_path .=  $language_url->prefix ?  $language_url->prefix . '/' : '';
 ?>
+<h1>language select</h1>
+<ul>
+    <li><a href="<?php echo $base_path . 'en/' . "admin/ironfolio/" . $current_cat_id ?>">English</a></li>
+    <li><a href="<?php echo $base_path . 'ru/' . "admin/ironfolio/" . $current_cat_id ?>">Русский</a></li>
+</ul>
+
+
+
 <h1>Cats</h1>
 <ul>
 <?php foreach ($categories as $cat): ?>
@@ -12,7 +20,9 @@ $current_url_path .=  $language_url->prefix ?  $language_url->prefix . '/' : '';
                $cat_class = 'active';
            }
     ?>
-    <li class="<?php echo $cat_class; ?>"><a href="<?php echo $current_url_path . "admin/ironfolio/$cat->tid"; ?>"><?php echo $cat->name; ?></a></li>
+    <li class="<?php echo $cat_class; ?>">
+        <a href="<?php echo $current_url_path . "admin/ironfolio/$cat->tid"; ?>"><?php echo $cat->name; ?></a>
+    </li>
 <?php endforeach; ?>
 </ul>
 
